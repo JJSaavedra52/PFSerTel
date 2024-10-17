1074499bf019b91c829c4b41988125723b9a84b3
Juan José Saavedra Realpe <juanjsaavedra52@gmail.com>
2024-10-17 13:26:09 -0500

    FIX: views and books component relation errors

--- webapp/web/views.py
+++ webapp/web/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from users.controllers.user_controller import user_controller
-from products.controllers.product_controller import product_controller  # Tu controlador de productos
+from books.controllers.book_controller import book_controller 
 from db.db import db
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ db.init_app(app)
 
 # Registrando los blueprints de los controladores
 app.register_blueprint(user_controller)
-app.register_blueprint(product_controller)  # Registrando el controlador de productos
+app.register_blueprint(book_controller)  # Registrando el controlador de productos
 
 # Ruta para renderizar el template index.html
 @app.route('/')
@@ -21,14 +21,14 @@ def edit_user(id):
     print("id recibido", id)
     return render_template('edit.html', id=id)
 
-@app.route('/products')
+@app.route('/books')
 def products():
-    return render_template('products.html')
+    return render_template('books.html')
 
-@app.route('/editproduct/<string:ref>')
-def edit_product(ref):
-    print("Referencia recibida", ref)
-    return render_template('editproduct.html', ref=ref)
+@app.route('/editbook/<string:ref>')
+def edit_product(code):
+    print("Referencia recibida", code)
+    return render_template('editbook.html', code=code)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')  # Escuchar en todas las direcciones IP
