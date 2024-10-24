105fad4049672da88a99a1e423a169826a2472c1
HexDan <123597766+Dan23H@users.noreply.github.com>
2024-10-24 15:20:57 -0500

    FIX: Apache config

--- script.sh
+++ script.sh
@@ -31,6 +31,45 @@ pip install Flask-SQLAlchemy
 echo "Installing Apache"
 sudo apt install apache2 libapache2-mod-wsgi-py3 -y
 
+# Enable mod_wsgi module
+echo "Enabling mod_wsgi module"
+sudo a2enmod wsgi
+
+# Copy webapp directory to /var/www
+echo "Copying webapp to /var/www"
+sudo cp -r /home/vagrant/webapp /var/www/webapp
+
+# Create application.wsgi file
+echo "Creating application.wsgi"
+sudo bash -c 'cat > /var/www/webapp/application.wsgi <<EOF
+#!/usr/bin/python
+import sys
+sys.path.insert(0,"/var/www/webapp/")
+from app import app as application
+EOF'
+
+# Configure Apache
+echo "Configuring Apache"
+sudo bash -c 'cat > /etc/apache2/sites-available/000-default.conf <<EOF
+<VirtualHost *:80>
+    WSGIScriptAlias / /var/www/webapp/application.wsgi
+    DocumentRoot /var/www/webapp
+    <Directory /var/www/webapp/>
+        Order deny,allow
+        Allow from all
+    </Directory>
+</VirtualHost>
+EOF'
+
+# Enable the site and restart Apache
+echo "Enabling site and restarting Apache"
+sudo a2ensite 000-default.conf
+sudo systemctl restart apache2
+
+# Check Apache configuration syntax
+echo "Checking Apache configuration syntax"
+sudo apachectl -t
+
 #Run application
 # cd /home/vagrant/webapp
 # export FLASK_APP=run.py
